28ec13b57ed945feaf7e072d1abacd417f233e26
tonidevj <bogadoantoni4@gmail.com>
2024-08-23 12:06:09 -0400

    first commit

---
+++ metodosnumericos/interfaces/Ifuntion.java
@@ -0,0 +1,8 @@
+package metodosnumericos.interfaces;
+
+
+public interface Ifuntion {
+    public abstract double eval(double x);
+//    public abstract double evalNewton(double ini, double iter);
+  //  public abstract double evalNewton2(String derivada, double ini, double iter);
+}
